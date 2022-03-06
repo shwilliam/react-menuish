5c5d60f4bdac3f5e595213c44417f9a9dbbcd638
William Lindvall <shwilliam@hey.com>
2022-03-06 16:40:33 +0100

    add nested modal and menu in modal stories

--- src/components/modal.stories.tsx
+++ src/components/modal.stories.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react'
 import { Modal, ModalContent } from './modal'
+import * as Menu from './menu'
 import { Lorem } from './lorem'
+import { fruits } from '../util/fruits'
 
 export default {
   title: 'Modal',
@@ -48,6 +50,46 @@ export const InitialFocus = () => {
           <button>not me</button>
           <button ref={initialFocusRef}>me</button>
           <button onClick={() => setIsOpen(false)}>close</button>
+        </ModalContent>
+      </Modal>
+    </div>
+  )
+}
+
+export const WithMenu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>open</button>
+      <Modal isOpen={isOpen}>
+        <ModalContent onClose={() => setIsOpen(false)}>
+          <Menu.Menu
+            trigger={({ anchorRef, open }) => (
+              <button ref={anchorRef} onClick={open}>
+                open
+              </button>
+            )}
+          >
+            <Menu.List>
+              {fruits.map((f) => (
+                <Menu.Item key={f}>{f}</Menu.Item>
+              ))}
+            </Menu.List>
+          </Menu.Menu>
+        </ModalContent>
+      </Modal>
+    </div>
+  )
+}
+
+export const Nested = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>open</button>
+      <Modal isOpen={isOpen}>
+        <ModalContent onClose={() => setIsOpen(false)}>
+          <Nested />
         </ModalContent>
       </Modal>
     </div>
